9199dc97c8a979ead54c7953ea67eed93c8fb9bc
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-02-23 21:53:51 +0100

    On hold

--- Youtube_Clone/src/App.jsx
+++ Youtube_Clone/src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Box } from '@mui/material'
 
-const App = () => {
-  return (
-    <div>Hello Taiwo, What are we making today?</div>
-  )
-}
+const App = () => (
+    <BrowserRouter>
+        <Box sx={{backgroundColor: '#000'}}>
+            <Navbar />
+            <Routes>
+                <Route path='/' exact element={<Feed />} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
+)
 
 export default App
